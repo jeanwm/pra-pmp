fc0169809861a48470ff9628cb08acaae8371e7d
Jean Ernani Witt Meier <71325720+jeanwm@users.noreply.github.com>
2024-09-19 20:09:24 -0300

    Add files via upload

---
+++ pra-pmp-pesseqsent1.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+int* criarVetor(int n) {
+	int i  = 0;
+	int *v = malloc((sizeof(int) * n) + 1);
+	
+	for (i = 0; i < n; i++) {
+		v[i] = rand() % n;
+	}
+	
+	return v;
+}
+
+int pesquisaSequencialSentinela(int chave, int v[], int n) {
+	int i = 0;
+	int contador = 1;
+	v[n] = chave;
+	
+	while(v[i] != chave) {
+		contador++;
+		i++;
+	}
+	
+	if (i < n) {
+		return contador;	
+	}
+	
+	return contador;
+}
+
+int main() {
+	int n = 10;
+	int* v = criarVetor(n);
+	
+	printf("melhor caso: %d\n", pesquisaSequencialSentinela(v[0], v, n));
+	printf("pior caso: %d\n", pesquisaSequencialSentinela(v[11], v, n));
+	printf("caso medio: %d\n", pesquisaSequencialSentinela(v[rand() % n], v, n));
+	
+	return 0;
+}
